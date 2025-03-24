a14679009d9526567fc0346fe436fd94afecc10c
Grittamet Wilai <132270499+grittametw@users.noreply.github.com>
2025-03-24 15:58:18 +0700

    Organize code
    - Organize code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,70 +1,72 @@
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
-import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import SupportAgentOutlinedIcon from '@mui/icons-material/SupportAgentOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import { Box, Grid2, Typography, List, ListItem } from '@mui/material';
+import {
+  HomeOutlined, CategoryOutlined, LocalLibraryOutlined, FavoriteBorderOutlined,
+  SettingsOutlined, SupportAgentOutlined, LogoutOutlined, Menu
+} from '@mui/icons-material';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Sidebar() {
 
   return (
-    <div>
-      <div className="d-flex flex-column flex-shrink-0 p-4" style={{ width: '300px', height: '100vh', backgroundColor: '#fff' }}>
-        <a href="/" className="d-flex align-items-center p-2 pb-4 gap-3">
-          <img src="icon_web.png" alt="" width={'48px'} />
-          Web - Library
-        </a>
 
-        <div className="nav-pills flex-column mb-auto">
-          <ul className="nav p-2 pb-4 gap-2" style={{ borderBottom: '1px solid #ebedf4' }}>
-            <li className="nav-item" style={{ width: '100%' }}>
-              <a href="#" className="nav-link active d-flex align-items-center gap-4" aria-current="page" style={{ fontSize: '24px' }}>
-                <HomeOutlinedIcon />
-                Home
-              </a>
-            </li>
-            <li style={{ width: '100%' }}>
-              <a href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px' }}>
-                <CategoryOutlinedIcon />
-                Category
-              </a>
-            </li>
-            <li style={{ width: '100%' }}>
-              <a href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px' }}>
-                <LocalLibraryOutlinedIcon />
-                My Library
-              </a>
-            </li>
-            <li style={{ width: '100%' }}>
-              <a href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px' }}>
-                <FavoriteBorderOutlinedIcon />
-                Favourite
-              </a>
-            </li>
-          </ul>
-          <ul className="nav p-2 pt-4gap-2" style={{ fontSize: '24px' }}>
-            <li style={{ width: '100%' }}>
-              <a href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px' }}>
-                <SettingsOutlinedIcon />
-                Settings
-              </a>
-            </li>
-            <li style={{ width: '100%' }}>
-              <a href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px' }}>
-                <SupportAgentOutlinedIcon />
-                Support
-              </a>
-            </li>
-            <li style={{ width: '100%' }}>
-              <a href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px' }}>
-                <LogoutOutlinedIcon />
-                Logout
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Box className="d-flex flex-column flex-shrink-0 p-4" style={{ width: '100%', maxWidth: 300, minHeight: '100vh', backgroundColor: '#fff' }}>
+      <Grid2 className="d-flex justify-content-between align-items-center p-2 pb-4">
+        <Link href="/" className="d-flex align-items-center gap-2">
+          <Image src="/icon_web.png" alt="" width={48} height={48} />
+          <Typography fontWeight={600} fontSize={20}>Web - Library</Typography>
+        </Link>
+        <Menu style={{ fontSize: '24px' }} />
+      </Grid2>
+
+      <Grid2 className="nav-pills flex-column mb-auto">
+        <List className="nav pb-4" style={{ borderBottom: '1px solid #ebedf4' }}>
+          <ListItem className="nav-item">
+            <Link href="#" className="nav-link active d-flex align-items-center gap-4" aria-current="page" style={{ fontSize: '24px', width: '100%' }}>
+              <HomeOutlined />
+              <Typography fontSize={24}>Home</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px', width: '100%' }}>
+              <CategoryOutlined />
+              <Typography fontSize={24}>Category</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px', width: '100%' }}>
+              <LocalLibraryOutlined />
+              <Typography fontSize={24}>My Library</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px', width: '100%' }}>
+              <FavoriteBorderOutlined />
+              <Typography fontSize={24}>Favourite</Typography>
+            </Link>
+          </ListItem>
+        </List>
+        <List className="nav pt-4" style={{ fontSize: '24px' }}>
+          <ListItem>
+            <Link href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px', width: '100%' }}>
+              <SettingsOutlined />
+              <Typography fontSize={24}>Settings</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px', width: '100%' }}>
+              <SupportAgentOutlined />
+              <Typography fontSize={24}>Support</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="#" className="nav-link d-flex align-items-center gap-4" style={{ fontSize: '24px', width: '100%' }}>
+              <LogoutOutlined />
+              <Typography fontSize={24}>Logout</Typography>
+            </Link>
+          </ListItem>
+        </List>
+      </Grid2>
+    </Box>
   );
 }
