c4b2bb2f5db9a7b5f2d785fa57b0c5dd6c71ea76
Grittamet Wilai <132270499+grittametw@users.noreply.github.com>
2026-01-14 15:15:28 +0700

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -11,7 +11,7 @@ export function getPool(): Pool {
         connectionString:
           process.env.POSTGRES_URL_NON_POOLING ||
           process.env.POSTGRES_URL ||
-          process.env.DATABASE_URL,
+          process.env.POSTGRES_PRISMA_URL,
         ssl: {
           rejectUnauthorized: false,
         },
