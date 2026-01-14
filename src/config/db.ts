771293c088f00ebf9d19d62068111828a739b68b
Grittamet Wilai <132270499+grittametw@users.noreply.github.com>
2026-01-14 15:41:14 +0700

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,30 +4,30 @@ let pool: Pool
 
 export function getPool(): Pool {
   if (!pool) {
-    const isProd = !!process.env.POSTGRES_URL_NON_POOLING
+    const isProd = process.env.NODE_ENV === 'production'
 
     const dbConfig: PoolConfig = isProd
       ? {
-        connectionString: process.env.POSTGRES_URL_NON_POOLING,
-        ssl: { rejectUnauthorized: false },
-        max: 10,
-        idleTimeoutMillis: 30000,
-        connectionTimeoutMillis: 2000,
-      }
+          connectionString: process.env.POSTGRES_URL_NON_POOLING,
+          ssl: { rejectUnauthorized: false },
+          max: 10,
+          idleTimeoutMillis: 30000,
+          connectionTimeoutMillis: 2000,
+        }
       : {
-        host: process.env.POSTGRES_HOST || 'localhost',
-        user: process.env.POSTGRES_USER || 'postgres',
-        password: process.env.POSTGRES_PASSWORD || 'postgres',
-        database: process.env.POSTGRES_DATABASE || 'postgres',
-        port: Number(process.env.POSTGRES_PORT) || 54322,
-        ssl:
-          process.env.POSTGRES_SSL_REJECT_UNAUTHORIZED === 'false'
-            ? false
-            : { rejectUnauthorized: true },
-        max: 10,
-        idleTimeoutMillis: 30000,
-        connectionTimeoutMillis: 2000,
-      }
+          host: process.env.POSTGRES_HOST || 'localhost',
+          user: process.env.POSTGRES_USER || 'postgres',
+          password: process.env.POSTGRES_PASSWORD || 'postgres',
+          database: process.env.POSTGRES_DATABASE || 'postgres',
+          port: Number(process.env.POSTGRES_PORT) || 54322,
+          ssl: {
+            rejectUnauthorized:
+              process.env.POSTGRES_SSL_REJECT_UNAUTHORIZED !== 'false',
+          },
+          max: 10,
+          idleTimeoutMillis: 30000,
+          connectionTimeoutMillis: 2000,
+        }
 
     pool = new Pool(dbConfig)
   }
